869b5a104a4875493a51d23eed3aa628b0d82326
Beatris Antunes Silva <79115923+beatrisantunes@users.noreply.github.com>
2022-11-09 16:35:01 -0300

    Aula 7 - Função III

---
+++ Javascript/Função III/funcao III.js
@@ -0,0 +1,10 @@
+// função anonima
+
+(function () {
+    console.log('Execução Automatica função anonima')
+})()//Autoexecução
+
+// funcao anonima setTimeout
+setTimeout(() => {
+    console.log("3s para ser executado");
+}, 3000);
