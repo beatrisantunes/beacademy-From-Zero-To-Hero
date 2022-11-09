baf3b1bbbe754b9ad801d1ee1f18b000a6bc833c
Beatris Antunes Silva <79115923+beatrisantunes@users.noreply.github.com>
2022-11-09 16:19:48 -0300

    Aula 3 - Variáveis II

--- Javascript/Variaveis II/variaveis II.js
+++ Javascript/Variaveis II/variaveis II.js
@@ -2,7 +2,7 @@
 
 
 const humano = {
-    nome: 'Bruno Travassos',
+    nome: 'Beatris Antunes Silva',
     idade: 36,
     peso: 100,
     ehAtleta: false,
@@ -16,7 +16,7 @@ console.log(humano.idade)
 console.log(humano.andar())
 
 //Concatenar 
-console.log("O humano "+ humano.nome + " tem a idade de "+humano.idade+" anos")
+console.log("O humano " + humano.nome + " tem a idade de " + humano.idade + " anos")
 
 //Interpolar
 console.log(`O humano ${humano.nome} tem a idade de ${humano.idade} anos`)
@@ -30,16 +30,15 @@ const animais = [
     {
         nome: 'Teco',
         idade: 2,
-        peso:0.3
+        peso: 0.3
     }
 ]
 console.log(animais)
-console.log(animais.length)//tamanho do array
-console.log(animais[2])//posicao 2
-console.log(animais[4])//posicao 4
-console.log(animais[4].nome)//posicao 4
+console.log(animais.length) //tamanho do array
+console.log(animais[2]) //posicao 2
+console.log(animais[4]) //posicao 4
+console.log(animais[4].nome) //posicao 4
 
 console.log(
-  `O animal ${animais[3]} tem o nome ${animais[4].nome} tem o peso de ${animais[4].peso} kg`
+    `O animal ${animais[3]} tem o nome ${animais[4].nome} tem o peso de ${animais[4].peso} kg`
 );
-
